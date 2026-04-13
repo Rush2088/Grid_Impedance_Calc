df82064ca0c64515a4419cb7cc7be08d90d0ef12
Rush2088 <rsd3k@yahoo.com>
2026-04-14 00:01:55 +1000

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+  base: '/Grid_Impedance_Calc/',
   plugins: [react()],
-  base: '/Grid Impedance Calculator/',
 });
